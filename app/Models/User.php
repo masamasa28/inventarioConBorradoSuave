32970b39fb7d3ab65ea4cb96cd8b2f8c9a44d2fb
maria.ortega <ortegamariafernanda3876@gmail.com>
2024-10-11 20:22:11 -0500

    Descripción clara de los cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,25 +1,18 @@
 <?php
 
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
-    use HasFactory;
-    use Notifiable;
-  
-    use HasRoles;
-
-    use HasApiTokens, HasFactory,Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -53,5 +46,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
